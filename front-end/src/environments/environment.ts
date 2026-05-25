75b8e6c3109153854ad7f8eca1e3cb3151419f9d
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-26 00:54:17 +0800

    Add environment

---
+++ front-end/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://eiizkgsiomaotixsemrx.supabase.co',
+  supabaseKey: 'sb_publishable_3VA7vmQqoERok-iT7E_UyQ_v338asdr',
+   appUrl:'http://localhost:4200'
+}
