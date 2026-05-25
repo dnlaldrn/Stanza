b4e6a6f5442484b95ee52745984e3644337d32b8
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-25 23:04:23 +0800

    Modify the environment

--- front-end/src/app/core/config/supabase.client.ts
+++ front-end/src/app/core/config/supabase.client.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
 
 export const supabase = createClient(
   environment.supabaseUrl,
