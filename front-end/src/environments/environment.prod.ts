47f7120824e944ba6ecf90bf33b74cdfb414bfff
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-26 00:52:11 +0800

    Remove the environment from git ignore

---
+++ front-end/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://eiizkgsiomaotixsemrx.supabase.co',
+  supabaseKey: 'sb_publishable_3VA7vmQqoERok-iT7E_UyQ_v338asdr',
+  appUrl: 'https://stanza-ph.vercel.app',
+};
