7a593479603df4ccf717a46f7c740f9cd3946fe8
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-30 15:23:33 +0800

    Add dashboard page and its routing

---
+++ front-end/src/app/features/pages/dashboard-page/dashboard-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {DashboardComponent} from '../../components/dashboard-component/dashboard-component'
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [DashboardComponent],
+  templateUrl: './dashboard-page.html',
+  styleUrl: './dashboard-page.css',
+})
+export class DashboardPage {}
