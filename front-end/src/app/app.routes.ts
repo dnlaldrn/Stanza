cb0f93fe46660f48035a7d36020c5feb966b2911
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-31 15:40:28 +0800

    update the routing

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -9,19 +9,19 @@ import { FeedbackPage } from './features/pages/feedback-page/feedback-page';
 import { PropertyDetailsPage } from './features/pages/property-details-page/property-details-page';
 import { PropertyDetailsComponent } from './features/components/property-details-component/owner-details-component';
 import { authGuard } from './core/auth/auth.guard';
-import {ProfilePage} from './features/pages/profile-page/profile-page'
-import {DashboardPage} from './features/pages/dashboard-page/dashboard-page';
+import { ProfilePage } from './features/pages/profile-page/profile-page';
+import { DashboardPage } from './features/pages/dashboard-page/dashboard-page';
 import { landlordGuard } from './core/auth/landlord.guard';
 
 export const routes: Routes = [
-  { path: '',  component: HomePage },
+  { path: '', component: HomePage },
   { path: 'about', component: AboutPage },
   { path: 'login', component: LoginPage },
   { path: 'sign-up', component: SignUpPage },
   { path: 'donation', component: DonationPage },
-  { path: 'listing', canActivate:[authGuard], component: ListingPage },
+  { path: 'listing', canActivate: [authGuard], component: ListingPage },
   { path: 'feedback', component: FeedbackPage },
-  { path: 'property/:propertyId', canActivate:[authGuard], component: PropertyDetailsPage},
-  {path: 'dashboard', canActivate:[landlordGuard], component: DashboardPage},
-  
+  { path: 'property/:propertyId', canActivate: [authGuard], component: PropertyDetailsPage },
+  { path: 'dashboard', canActivate: [landlordGuard], component: DashboardPage },
+  { path: 'profile', component: ProfilePage },
 ];
