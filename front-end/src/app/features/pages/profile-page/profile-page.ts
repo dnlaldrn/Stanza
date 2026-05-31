dc91ca93f7eb4231a8166c3aa4998efbe06c6efd
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-31 15:40:51 +0800

    Add view profile ui

--- front-end/src/app/features/pages/profile-page/profile-page.ts
+++ front-end/src/app/features/pages/profile-page/profile-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {ProfileComponent} from '../../components/profile-component/profile-component'
 
 @Component({
   selector: 'app-profile-page',
-  imports: [],
+  imports: [ProfileComponent],
   templateUrl: './profile-page.html',
   styleUrl: './profile-page.css',
 })
