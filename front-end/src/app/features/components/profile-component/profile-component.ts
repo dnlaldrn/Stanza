0d9b0129bb55700e05d7466f0b5a616a7f1541db
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-06-01 18:36:58 +0800

    Add profile component ui

--- front-end/src/app/features/components/profile-component/profile-component.ts
+++ front-end/src/app/features/components/profile-component/profile-component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, inject } from '@angular/core';
+import {AuthService} from '../../../core/auth/auth.service'
 
 @Component({
   selector: 'app-profile-component',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './profile-component.html',
   styleUrl: './profile-component.css',
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  isEditingPersonal = false;
+  isEditingPassword = false;
+  isEditingEmail= false;
+
+  public auth = inject(AuthService);
+}
+
